7c7922ac59f7d7c207971631f439ccb2a23786c7
Matwindu <Matthieu059@hotmail.fr>
2026-03-13 23:48:47 +0100

    Fix syntax error and real map rendering

--- public/territories-data.js
+++ public/territories-data.js
@@ -1,6 +1,6 @@
 // Données cartographiques avec tracés SVG réels
 window.TERRITORIES = {
-  // --- NORTH AMERICA ---
+  // AMÉRIQUE DU NORD
   'alaska': { 
     name: 'Alaska', continent: 'north_america', x: 80, y: 70, 
     path: 'M46.7,63.1 L49.2,59.3 L55.5,58.4 L61.7,53.2 L63.6,56.7 L69.8,55.8 L74.3,64.2 L83.2,64.7 L85.9,71.2 L96.3,71.7 L99.1,79.5 L106.9,80.9 L109.1,88.4 L114.3,87.9 L115.1,95.5 L125,95.9 L125.8,103.5 L135.5,103.8 L136.3,111.4 L146.4,111.7 L147.1,120.3 L157.1,120.7 L157.9,131.2 L167.9,131.5 L168.6,141.5 L151,141.5 L144.1,133.4 L138,133 L134.5,138 L124.6,134.9 L117.8,134.5 L113.1,138 L103,136.6 L96.3,137.5 L91.6,140.6 L85,137.9 L77.8,140.6 L72,138 L65.3,142 L60.4,142 Z' 
@@ -18,13 +18,13 @@ window.TERRITORIES = {
     path: 'M254,85.5 L325.4,85.5 L325.4,160.2 L254,160.2 Z' 
   },
 
-  // --- SOUTH AMERICA ---
+  // AMÉRIQUE DU SUD
   'brazil': { 
     name: 'Brazil', continent: 'south_america', x: 315, y: 315, 
     path: 'M265.4,245.5 L350.2,245.5 L385.1,290.4 L360.5,380.2 L280.1,385.5 L245.4,320.4 Z' 
   },
 
-  // --- EUROPE ---
+  // EUROPE
   'great_britain': { 
     name: 'G. Bretagne', continent: 'europe', x: 415, y: 135, 
     path: 'M405.5,120.4 L425.2,120.4 L425.2,155.5 L405.5,155.5 Z' 
@@ -34,7 +34,7 @@ window.TERRITORIES = {
     path: 'M395.4,175.5 L435.2,175.5 L435.2,215.5 L395.4,215.5 Z' 
   },
 
-  // --- AFRICA ---
+  // AFRIQUE
   'egypt': { 
     name: 'Égypte', continent: 'africa', x: 525, y: 245, 
     path: 'M505.4,225.5 L545.2,225.5 L545.2,265.5 L505.4,265.5 Z' 
